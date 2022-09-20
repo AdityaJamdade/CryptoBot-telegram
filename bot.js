55bef9bc027ebf7ad5f891027ca794eb33af8296
AdityaJamdade <adijamdade156@gmail.com>
2022-09-20 22:02:27 +0530

    added the fetch data functionality for each cryptocurrency

--- bot.js
+++ bot.js
@@ -10,64 +10,97 @@ const apikey = process.env.CRYPTO_COMPARE_API_KEY;
 const bot = new Telegraf(TOKEN);
 
 // now bot is a telegraf object and can access all the bot methods
-bot.command('start', ctx => {
+
+const sendStartMessage = (ctx) => {
     const chat_id = ctx.chat.id;
     let startMessage = `Welcome, this bot is useful for getting the cryptocurrency information!`
-    bot.telegram.sendMessage(chat_id,  startMessage, 
+    bot.telegram.sendMessage(chat_id, startMessage,
         {
-            reply_markup:{
-                inline_keyboard:[
+            reply_markup: {
+                inline_keyboard: [
                     [
-                        {text:"Crypto Prices", callback_data:'price'}
+                        { text: "Crypto Prices", callback_data: 'price' }
                     ],
                     [
-                        {text:"CoinMarketCap", url: 'https://coinmarketcap.com/'}
+                        { text: "CoinMarketCap", url: 'https://coinmarketcap.com/' }
                     ]
                 ]
             }
         })
+}
+
+bot.command('start', ctx => {
+    sendStartMessage(ctx);
+})
+
+bot.action('start', ctx => {
+    ctx.deleteMessage();
+    sendStartMessage(ctx);
 })
 
 bot.action('price', ctx => {
     const chat_id = ctx.chat.id;
     let priceMessage = `Get price Information! select any of the following cryptocurrencies.`;
     ctx.deleteMessage();
-    bot.telegram.sendMessage(chat_id, priceMessage, 
+    bot.telegram.sendMessage(chat_id, priceMessage,
         {
-            reply_markup:{
-                inline_keyboard:[
+            reply_markup: {
+                inline_keyboard: [
                     [
-                        {text:"BTC", callback_data:'price-BTC'},
-                        {text:"ETH", callback_data:'price-ETH'}
+                        { text: "BTC", callback_data: 'price-BTC' },
+                        { text: "ETH", callback_data: 'price-ETH' }
                     ],
                     [
-                        {text:"BCH", callback_data:'price-BCH'},
-                        {text:"LTC", callback_data:'price-LTC'}
+                        { text: "BCH", callback_data: 'price-BCH' },
+                        { text: "LTC", callback_data: 'price-LTC' }
                     ],
                     [
-                        {text:"Back to Menu", callback_data: 'start'}
-                    ]
-                ]
-            } 
-        })
-})
-
-bot.action('start', ctx => {
-    const chat_id = ctx.chat.id;
-    let startMessage = `Welcome, this bot is useful for getting the cryptocurrency information!`
-    bot.telegram.sendMessage(chat_id,  startMessage, 
-        {
-            reply_markup:{
-                inline_keyboard:[
-                    [
-                        {text:"Crypto Prices", callback_data:'price'}
-                    ],
-                    [
-                        {text:"CoinMarketCap", url: 'https://coinmarketcap.com/'}
+                        { text: "Back to Menu", callback_data: 'start' }
                     ]
                 ]
             }
         })
+})
+
+let priceActionList = ['price-BTC', 'price-ETH', 'price-BCH', 'price-LTC']
+
+bot.action(priceActionList, async (ctx) => {
+    const chat_id = ctx.chat.id;
+    let symbol = ctx.match[0].split('-')[1];
+    console.log(symbol);
+
+    try {
+        let res = await axios.get(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${symbol}&tsyms=USD,INR&api_key=${apikey}`);
+        let usdData = res.data.DISPLAY[symbol].USD;
+        let inrData = res.data.DISPLAY[symbol].INR;
+        console.log(usdData);
+
+        let message =
+            `
+        Symbol: ${symbol}\n
+        Price: ${usdData.PRICE}
+        Open: ${usdData.OPENDAY}
+        High: ${usdData.HIGHDAY}
+        Low: ${usdData.LOWDAY}
+        Supply: ${usdData.SUPPLY}
+        Market Cap: ${usdData.MKTCAP}
+        `;
+
+        ctx.deleteMessage();
+        bot.telegram.sendMessage(chat_id, message,
+            {
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            { text: "Back to prices", callback_data: 'price' }
+                        ]
+                    ]
+                }
+            })
+    } catch (error) {
+        console.log(error);
+        ctx.reply("Some error occurred!")
+    }
 })
 
 
