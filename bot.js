f0cba61a3eb6618021c3f346fcd922a4ebfc262c
AdityaJamdade <adijamdade156@gmail.com>
2022-09-20 20:45:53 +0530

    demonstrated how to use inine keyboard

--- bot.js
+++ bot.js
@@ -10,6 +10,70 @@ const bot = new Telegraf(TOKEN);
 
 // now bot is a telegraf object and can access all the bot methods
 
+bot.command(('test', ctx => {
+    bot.telegram.sendMessage(ctx.chat.id, 'Main Menu',
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'Fruits', callback_data: 'fruits' }
+                    ],
+                    [
+                        { text: 'Vegetables', callback_data: 'vegetables' }
+                    ]
+                ]
+            }
+        })
+}))
+
+bot.action('fruits', ctx => {
+    const id = ctx.chat.id;
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(id, `List of Fruits\n\n1.Apples\n2.Bananas`,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'Back to Menu', callback_data: 'menu' }
+                    ]
+                ]
+            }
+        })
+})
+
+bot.action('vegetables', ctx => {
+    const id = ctx.chat.id;
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(id, `List of Vegetables\n\n1.Potatoes\n2.Lady Fingers`,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'Back to Menu', callback_data: 'menu' }
+                    ]
+                ]
+            }
+        })
+})
+
+bot.action('menu', ctx => {
+    const id = ctx.chat.id;
+    ctx.deleteMessage();
+    bot.telegram.sendMessage(ctx.chat.id, 'Main Menu',
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: 'Fruits', callback_data: 'fruits' }
+                    ],
+                    [
+                        { text: 'Vegetables', callback_data: 'vegetables' }
+                    ]
+                ]
+            }
+        })
+
+})
 
 
 // command for launching the bot
